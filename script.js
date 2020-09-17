14f3a2a01a31db422d347a21e980cb6abe5bb59e
jleilani <jxleilani@gmail.com>
2020-09-17 14:35:49 -0400

    fixed navbar functionality

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-/* Initialize Materialize */
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.fixed-action-btn');
-  var instances = M.FloatingActionButton.init(elems, {
-      direction: 'right',
-      hoverEnabled: false
-  });
-});
-
 /* Date Elements using Moment.js */
 $("#todays-date").text(moment().format('dddd') +" "+ moment().format('L'));
 $("#week-range").text(moment().startOf('week').format('L') +" - " + moment().endOf('week').format('L'));
