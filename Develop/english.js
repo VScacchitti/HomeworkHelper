bd564c13b3b164381f4d6ee6057e92f896d92c21
Vince Scacchitti <VScacchitti24@gmail.com>
2020-09-18 16:16:26 -0400

    APIs are working currently, will write a future clear function.

--- Develop/english.js
+++ Develop/english.js
@@ -1,51 +1,70 @@
 $(document).ready(function () {
   console.log("test");
 
-  var word = document.getElementById("input").innerText;
-
+  var inputVal = $("#input");
 
   //Dictionary API
-  var settings = {
-    async: true,
-    crossDomain: true,
-    url:
-      "https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=word" +
-      word,
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "twinword-word-graph-dictionary.p.rapidapi.com",
-      "x-rapidapi-key": "c652f38724msh0d9bd2b60906f32p19cabbjsn51b20672f98d",
-    },
-  };
+  $("#dictionaryBtn").on("click", function (event) {
+    event.preventDefault();
+    var searchWord = inputVal.val();
+    console.log(searchWord);
+    //API Call
+    var settings = {
+      async: true,
+      crossDomain: true,
+      url:
+        "https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=" +
+        searchWord,
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "twinword-word-graph-dictionary.p.rapidapi.com",
+        "x-rapidapi-key": "c652f38724msh0d9bd2b60906f32p19cabbjsn51b20672f98d",
+      },
+    };
 
-  $.ajax(settings).done(function (response) {
-    var definition = JSON.stringify(response.meaning);
-    console.log(definition);
-    $("#dictionary").append("<p>" + definition + "</p>");
+    $.ajax(settings).done(function (response) {
+      var definition = JSON.stringify(response.meaning);
+      console.log(definition);
+      var definitionDisplay = [];
+      definitionDisplay.push(definition);
+      console.log(definitionDisplay);
+      $("#dictionary").append("<p>" + definitionDisplay + "</p>");
+    });
   });
 
-
-
   //GrammerBot API
-  var settings2 = {
-    async: true,
-    crossDomain: true,
-    url: "https://grammarbot.p.rapidapi.com/check",
-    method: "POST",
-    headers: {
-      "x-rapidapi-host": "grammarbot.p.rapidapi.com",
-      "x-rapidapi-key": "c652f38724msh0d9bd2b60906f32p19cabbjsn51b20672f98d",
-      "content-type": "application/x-www-form-urlencoded",
-    },
-    data: {
-      language: "en-US",
-      text: "i have a dog",
-    },
-  };
+  var englishbodyVal = $("#inputgram");
 
-  $.ajax(settings2).done(function (response) {
-    console.log(settings2.data.text);
-    console.log(response.matches);
-    $("#grammar").append("<p>" + definition + "</p>");
+  $("#grammerBtn").on("click", function (event) {
+    event.preventDefault();
+    var textVal = englishbodyVal.val();
+    console.log(textVal);
+
+    var settings2 = {
+      async: true,
+      crossDomain: true,
+      url: "https://grammarbot.p.rapidapi.com/check",
+      method: "POST",
+      headers: {
+        "x-rapidapi-host": "grammarbot.p.rapidapi.com",
+        "x-rapidapi-key": "c652f38724msh0d9bd2b60906f32p19cabbjsn51b20672f98d",
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      data: {
+        language: "en-US",
+        text: textVal,
+      },
+    };
+
+    $.ajax(settings2).done(function (response) {
+      console.log(settings2.data.text);
+      var results = JSON.stringify(response.matches);
+      var displayGrammer = [];
+      console.log(results);
+      displayGrammer.push(results);
+      console.log(displayGrammer);
+
+      $("#grammer").append("<p>" + displayGrammer + "</p>");
+    });
   });
 });
